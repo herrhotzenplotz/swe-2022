fc85bf5e45f98ed8d114b354d39291aef0083406
Nico Sonack <nsonack@herrhotzenplotz.de>
2022-05-30 16:43:46 +0200

    Kommentare für Aufgabe 2

--- swe-03/aufgabe2a/Energiewesen.cs
+++ swe-03/aufgabe2a/Energiewesen.cs
@@ -2,10 +2,26 @@ using System;
 
 namespace aufgabe2
 {
+    /* Enumerations-Datentyp:
+     *
+     * Diese Datentypen sind für Kategorien besonders geeignet.
+     */
     public enum Kategorie { THERMO, ELEKTRO, NONE }
 
+    /* Die Klasse Energiewesen.
+     *
+     * Eine Klasse ist eine Vorlage für ein Objekt. So erzeugen wir in
+     * Program.Main() zwei Objekte Wesen1 und Wesen2 der Klasse
+     * Energiewesen. Sie haben also die gleichen Methoden (Funktionen)
+     * und Eigenschaften.
+     *
+     * Ein Objekt einer Klasse wird mit dem Konstruktor (constructor)
+     * erstellt. Dieser ist eine Funktion, die auch Argumente annehmen
+     * kann. */
     public class Energiewesen
     {
+	/* Der Default-Konstruktor. Er nimmt keine Argumente an und
+	 * weist dem Energiewesen sinnvolle Default-Werte zu. */
 	public Energiewesen()
 	{
 	    m_Bezeichnung = "Unbenanntes Wesen";
@@ -14,6 +30,8 @@ namespace aufgabe2
 	    m_Leistung    = Double.NaN;
 	}
 
+	/* Ein alternativer Konstruktor, der 4 Argumente annimmt und
+	 * diese verarbeitet, um das Objekt zu initialisieren. */
 	public Energiewesen(string Bezeichnung, int RegNummer, Kategorie Kateg, double Leistung)
 	{
 	    m_Bezeichnung = Bezeichnung;
@@ -22,13 +40,20 @@ namespace aufgabe2
 	    m_Leistung    = Leistung;
 	}
 
-	/* Private interne Datenfelder der Klasse */
+	/* Private interne Datenfelder der Klasse.
+	 *
+	 * Diese sind privat, sodass wir von außerhalb der Klasse
+	 * (z.B. in der Main-Methode) nicht auf den internen Zustand
+	 * zugreifen können und diesen modifizieren können. */
 	private string    m_Bezeichnung;
 	private int       m_RegNummer;
 	private Kategorie m_Kateg;
 	private double    m_Leistung;
 
-	/* Jeweils Getter- und Setter-Methoden für die Datenfelder */
+	/* Jeweils Getter- und Setter-Methoden für die Datenfelder
+	 *
+	 * - Setter-Methoden setzen das internen Datenfeld auf den übergebenen Wert.
+	 * - Getter-Methoden geben den aktuellen Wert des internen Datenfelds zurück. */
 	public void SetBezeichnung(string Bezeichnung) { m_Bezeichnung = Bezeichnung; }
 	public string GetBezeichnung() { return m_Bezeichnung; }
 
@@ -41,6 +66,7 @@ namespace aufgabe2
 	public void SetLeistung(double Leistung) { m_Leistung = Leistung; }
 	public double GetLeistung() { return m_Leistung; }
 
+	/* Nur eine Ausgaberoutine */
 	public void Print()
 	{
 	    Console.WriteLine("Wesen {{ Bezeichnung = {0}, RegNummer = {1}, Kategorie = {2}, Leistung = {3} }}",
--- swe-03/aufgabe2a/Program.cs
+++ swe-03/aufgabe2a/Program.cs
@@ -1,18 +1,26 @@
 using System;
 
+/* Diese Aufgabe ist in zwei Dateien aufgeteilt:
+ *
+ *   - Program.cs
+ *   - Energiewesen.cs
+ *
+ * Der interessante Teil ist in Letzterer zu finden. */
+
 namespace aufgabe2
 {
     class Program
     {
 	static void Main(string[] args)
 	{
+	    /* Den alternativen Konstruktor aufrufen */
 	    Energiewesen Wesen1 = new Energiewesen(
 		"Wesen 1", 42, Kategorie.THERMO, 3.14);
+
+	    /* Default-Konstruktor */
 	    Energiewesen Wesen2 = new Energiewesen();
 
-	    Wesen2.SetLeistung(2.72);
-	    Wesen2.SetKategorie(Kategorie.ELEKTRO);
-
+	    /* Beide Objekte ausgeben */
 	    Wesen1.Print();
 	    Wesen2.Print();
 	}
